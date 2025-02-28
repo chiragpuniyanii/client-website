1727f4453c7502f7b37d9583c98f7fa529334b2f
Chirag Puniyani <140302100+chiragpuniyanii@users.noreply.github.com>
2025-02-28 16:31:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
 echo "<h1>Welcome to Production Server!</h1>";
 echo "<h2>Testing Auto-Deployment!</h2>";
 echo "<h1>Your Website Runs Successfully!</h1>"
+echo "<h2>Test</h2>";
+echo "<h1>Your Website Runs</h1>"
 ?>
+
